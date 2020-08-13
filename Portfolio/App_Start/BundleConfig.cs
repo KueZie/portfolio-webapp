ac4912c4153cd8b931db97d1281f3816e7ffdf7f
KueZster <huntergoram2@gmail.com>
2020-08-13 02:58:04 -0400

    Initial commit.

---
+++ Portfolio/App_Start/BundleConfig.cs
@@ -0,0 +1,56 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Portfolio
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/vendor/jquery/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-easing").Include(
+                      "~/Scripts/vendor/jquery.easing/jquery.easing.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-validate").Include(
+                      "~/Scripts/vendor/jquery.validate/jquery.validate.min.js",
+                      "~/Scripts/vendor/jquery.validate/jquery.validate.unobtrusive.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/vendor/js/bootstrap.bundle.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/waypoints").Include(
+                      "~/Scripts/vendor/waypoints/jquery.waypoints.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/counterup").Include(
+                      "~/Scripts/vendor/counterup/counterup.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/isotope-layout").Include(
+                      "~/Scripts/vendor/isotope-layout/isotope.pkgd.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/venobox").Include(
+                      "~/Scripts/vendor/venobox/venobox.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owlcarousel").Include(
+                      "~/Scripts/vendor/owl.carousel/owl.carousel.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/typedjs").Include(
+                      "~/Scripts/vendor/typed.js/typed.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/aos").Include(
+                      "~/Scripts/vendor/aos/aos.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/css/style.css",
+                      "~/Content/vendor/venobox/venobox.css",
+                      "~/Content/vendor/owl.carousel/assets/owl.carousel.min.css",
+                      "~/Content/vendor/aos/aos.css"));
+        }
+    }
+}
---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Portfolio.Models;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using DataLibrary;
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ContactInfoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                JsonKeyReader reader = new JsonKeyReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "secrets.json"));
+                //SendGridClient client = new SendGridClient(
+                //    reader.LoadJsonKey(
+                //        "SendGridServiceAPIKey", (Exception e) => Console.WriteLine(e.Message)
+                //        )
+                //    );
+
+                SendGridClient client = new SendGridClient(
+                    reader.LoadJsonKey(
+                        "SendGridServiceAPIKey", (Exception e) => Console.WriteLine(e.Message)
+                        )
+                    );
+                EmailAddress from = new EmailAddress("portfolioemailer@gmail.com", "Portfolio Emailer");
+                EmailAddress to = new EmailAddress("huntergoram2@gmail.com");
+
+                SendGridMessage msg = MailHelper.CreateSingleEmail(
+                    from, to, model.Subject,
+                    model.Message + "\n\n" + model.EmailAddress,
+                    ""
+                );
+                client.SendEmailAsync(msg);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
