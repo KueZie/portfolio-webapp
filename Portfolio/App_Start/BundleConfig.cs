7de65aa7d1c60e47b0135d070e034e8ea0f160b0
KueZster <huntergoram2@gmail.com>
2020-08-14 03:41:39 -0400

    Release build

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace Portfolio
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/vendor/jquery/jquery.min.js"));
 
@@ -47,11 +49,18 @@ namespace Portfolio
 
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css.map",
+                      "~/Content/vendor/bootstrap/css/bootstrap.css.map",
                       "~/Content/css/style.css",
                       "~/Content/vendor/venobox/venobox.css",
                       "~/Content/vendor/owl.carousel/assets/owl.carousel.min.css",
                       "~/Content/vendor/aos/aos.css",
-                      "~/Content/vendor/icofont/icofont.min.css"));
+                      "~/Content/vendor/icofont/icofont.min.css",
+                      "~/Content/vendor/icofont/fonts/icofont.woff",
+                      "~/Content/vendor/icofont/fonts/icofont.woff2"));
+
+            bundles.Add(new StyleBundle("~/bundles/errpage-css").Include(
+                "~/Content/css/error-page-style.css"));
         }
     }
 }
