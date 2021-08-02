4b27ec32edffc085addfe355e8edc3d503a4e166
KueZie <huntergoram2@gmail.com>
2021-08-02 02:29:17 -0400

    Setup for GitHub GraphQL Api call

---
+++ api/graphql_client.py
@@ -0,0 +1,13 @@
+from gql import Client
+from gql.transport.requests import RequestsHTTPTransport
+from django.conf import settings
+
+git_client = Client(
+    transport=RequestsHTTPTransport(
+        url='https://api.github.com/graphql',
+        headers={'Authorization': f'bearer {settings.GITHUB_API_TOKEN}'},
+        verify=False,
+        retries=3,
+    ),
+    fetch_schema_from_transport=True,
+)
